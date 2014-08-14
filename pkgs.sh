943c24f275e82ed646bd4ad60cc4b2f8e13512ec
mb <mathieu.bivert@gmail.com>
2014-08-14 21:12:50 +0200

    comments; allows to build a single pkg when $1 is set

--- pkgs.sh
+++ pkgs.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
+# Directory where to store sources
 SRCDIR=$HOME/src
+# Where to store built packages
 PKGDIR=$HOME/pkgs
+# Configuration files
 FILES=$PWD/files
+
+# Packages descriptions are searched in
+# $PWD/pkgs; and moved to $PWD/pkgs.done when
+# processed.
 
 export SRCDIR PKGDIR FILES
 
 mkdir -p $SRCDIR || exit 1
 mkdir -p $PKGDIR || exit 1
-mkdir -p "$PKGDIR".done || exit 1
+mkdir -p "$PWD/pkgs.done" || exit 1
 
-for p in $PWD/pkgs/*; do
+for p in $PWD/pkgs/$1*; do
 	unset fetch; unset build; unset mkpkg; unset clean
 	source $p
-	fetch && build && mkpkg && clean && mv $p "$PKGDIR".done
+	fetch && build && mkpkg && clean && mv $p "$PWD/pkgs.done/"
 done
